81076e7ba51c5ea7414787bf36c93b638cc10033
adisuartama <48642051+adisuartama@users.noreply.github.com>
2019-03-30 21:30:00 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Cuaca from './src/04API/WeatherAsycn126';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Cuaca />
+    );
+  }
+}
